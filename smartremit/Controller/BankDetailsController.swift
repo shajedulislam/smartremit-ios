a5c5d9243a1a2a691de03b50dfa352f0fd8eea6f
Shajedul Islam <shajedul.islam@naztech.us.com>
2019-02-28 21:51:10 +0600

    picker added for all bank details screen choose buttons

--- smartremit/Controller/BankDetailsController.swift
+++ smartremit/Controller/BankDetailsController.swift
@@ -9,68 +9,105 @@
 import UIKit
 
 class BankDetailsController: UIViewController{
-    @IBOutlet weak var choosePurposeLabel: UILabel!
-    @IBOutlet weak var choosePurposeBtn: UIButton!
     
-    @IBOutlet weak var optionPicker: UIPickerView!
     @IBOutlet weak var menuBtn: UIButton!
-    @IBOutlet weak var choosePurposeTfield: UITextField!
     
-    var choosePurpose = ["Family Support", "Abc", "Dfg"]
-    
-    var optionStore = ""
+    @IBOutlet weak var choosePurposeLabel: UILabel!
+    @IBOutlet weak var chooseMethodLabel: UILabel!
+    @IBOutlet weak var chooseBankLabel: UILabel!
+    @IBOutlet weak var chooseDistrictLabel: UILabel!
+    @IBOutlet weak var chooseBranchLabel: UILabel!
+    @IBOutlet weak var chooseAccountLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //createPickerView()
-        //dissmissPickerVIew()
-        
         menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.rightRevealToggle(_:)), for: .touchUpInside)
 
-        // Do any additional setup after loading the view.
     }
+  
     
-    
-    
-    /*func createPickerView()
-    {
-        let pickerview = UIPickerView()
-        pickerview.delegate = self
-        choosePurposeTfield.inputView = pickerview
-    }
-    
-    func dissmissPickerVIew()
-    {
-        let toolbar = UIToolbar()
-        toolbar.sizeToFit()
-        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(self.dissmissKeyboard))
-        toolbar.setItems([doneButton], animated: false)
-        toolbar.isUserInteractionEnabled = true
-        
-        choosePurposeTfield.inputAccessoryView = toolbar
-       
-     
-    }
-    @objc func dissmissKeyboard()
-    {
-        view.endEditing(true)
-    }*/
-    
-    @IBAction func continueBtnPressed(_ sender: Any) {
-        self.revealViewController().rightViewController.performSegue(withIdentifier: "RemittanceSum", sender: self.revealViewController().rightViewController)
-    }
-    
+    //------------------Button Action Functions
     
     @IBAction func BackBtnToBenList(_ sender: Any) {
         
         self.revealViewController().rightViewController.performSegue(withIdentifier: "BeneficiaryList", sender: self.revealViewController().rightViewController)
     }
     
-    
-    
-    @IBAction func choosePurposeTapped(_ sender: Any) {
+    @IBAction func chooseForAllBtnTapped(_ sender: UIButton) {
         
+        if(sender.titleLabel?.text == "purpose")
+        {
+            optionReceiver(optionName: "purpose")
+        }
+        else if(sender.titleLabel?.text == "method")
+        {
+            optionReceiver(optionName: "method")
+        }
+        else if(sender.titleLabel?.text == "bank")
+        {
+            optionReceiver(optionName: "bank")
+        }
+        else if(sender.titleLabel?.text == "district")
+        {
+            optionReceiver(optionName: "district")
+        }
+        else if(sender.titleLabel?.text == "branch")
+        {
+            optionReceiver(optionName: "branch")
+        }
+        else if(sender.titleLabel?.text == "account")
+        {
+            optionReceiver(optionName: "account")
+        }
         
     }
+    
+    @IBAction func continueBtnPressed(_ sender: Any) {
+        self.revealViewController().rightViewController.performSegue(withIdentifier: "RemittanceSum", sender: self.revealViewController().rightViewController)
+    }
+    
+    //----------------Custom Functiins
+    
+    func optionReceiver(optionName : String) {
+
+        let sb = UIStoryboard(name: "BankDetailsOptionPopup", bundle: nil)
+        
+        let popup = sb.instantiateInitialViewController()! as! BankDetailsOptionPopupController
+        popup.optionToShow = optionName
+        
+        self.present(popup, animated: true)
+        
+        popup.onDone = { (data) in
+       
+        
+        if(optionName == "purpose")
+        {
+            self.choosePurposeLabel.text = data
+        }
+        else if(optionName == "method")
+        {
+            self.chooseMethodLabel.text = data
+        }
+        else if(optionName == "bank")
+        {
+            self.chooseBankLabel.text = data
+        }
+        else if(optionName == "district")
+        {
+            self.chooseDistrictLabel.text = data
+        }
+        else if(optionName == "branch")
+        {
+            self.chooseBranchLabel.text = data
+        }
+        else if(optionName == "account")
+        {
+            self.chooseAccountLabel.text = data
+        }
+            
+
+        }
+    }
+    
 }
---
+++ smartremit/Controller/BankDetailsOptionPopupController.swift
@@ -0,0 +1,102 @@
+//
+//  BankDetailsOptionPopupController.swift
+//  smartremit
+//
+//  Created by Shajedul Islam on 28/2/19.
+//  Copyright © 2019 NazTech. All rights reserved.
+//
+
+import UIKit
+
+class BankDetailsOptionPopupController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var popupHeaderText: UILabel!
+    var optionToShow : String = ""
+    
+    var optionList = [""]
+    
+    var choosenOption : String = ""
+    
+    var onDone: ((_ data: String) -> ())?
+    
+    //let methodList = ["To Bank Account","Cash Pick-up (PIN)"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if(optionToShow == "purpose")
+        {
+            popupHeaderText.text = "Choose Purpose"
+            optionList = ["Family Support","Another value","Another value"]
+            choosenOption = optionList[0]
+        }
+        else if(optionToShow == "method")
+        {
+            popupHeaderText.text = "Choose Payment Method"
+            optionList = ["To Bank Account","Cash Pick-up (PIN)"]
+            choosenOption = optionList[0]
+        }
+        else if(optionToShow == "bank")
+        {
+            popupHeaderText.text = "Choose Bank"
+            optionList = ["Southeast Bank Ltd.", "City Bank", "Brac Bank"]
+            choosenOption = optionList[0]
+        }
+        else if(optionToShow == "district")
+        {
+            popupHeaderText.text = "Choose District"
+            optionList = ["Dhaka","Chandpur","Comilla"]
+            choosenOption = optionList[0]
+        }
+        else if(optionToShow == "branch")
+        {
+            popupHeaderText.text = "Choose Branch"
+            optionList = ["Dhaka Banani Branch","Dhaka Gulshan Branch","Chandpur Branch"]
+            choosenOption = optionList[0]
+        }
+        else if(optionToShow == "account")
+        {
+            popupHeaderText.text = "Choose Account"
+            optionList = ["12345","54321"]
+            choosenOption = optionList[0]
+        }
+    }
+    
+    
+    
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return optionList.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return optionList[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        choosenOption = optionList[row]
+        
+    }
+    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        let myTitle = NSAttributedString(string: optionList[row], attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        
+        return myTitle
+    }
+    
+    
+    
+
+  
+    @IBAction func doneSelectingOption(_ sender: Any) {
+        
+        onDone?(choosenOption)
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
