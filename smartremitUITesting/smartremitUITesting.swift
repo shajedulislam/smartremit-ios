2940bf566744ab3a9e4abe7be5d0e4cd6c545e99
Shajedul Islam <shajedul.islam@naztech.us.com>
2019-03-03 13:20:04 +0600

    UI testing for valid login done

--- smartremitUITesting/smartremitUITesting.swift
+++ smartremitUITesting/smartremitUITesting.swift
@@ -1,6 +1,6 @@
 //
-//  smartremitUITests.swift
-//  smartremitUITests
+//  smartremitUITesting.swift
+//  smartremitUITesting
 //
 //  Created by Shajedul Islam on 3/3/19.
 //  Copyright © 2019 NazTech. All rights reserved.
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class smartremitUITests: XCTestCase {
+class smartremitUITesting: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -21,14 +21,38 @@ class smartremitUITests: XCTestCase {
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
+    
+    func testLoginSucess()
+    {
+        let validUsername = "shajedul"
+        let validPassword = "1234"
+        
+        
+        
+        let app = XCUIApplication()
+        
+        
+        app.buttons["Get Started"].tap()
+        
+        let usernameTextField = app.textFields["Username"]
+        XCTAssertTrue(usernameTextField.exists)
+        usernameTextField.tap()
+        usernameTextField.typeText(validUsername)
+        
+        let passwordTextField = app.secureTextFields["Password"]
+        XCTAssertTrue(passwordTextField.exists)
+        passwordTextField.tap()
+        passwordTextField.typeText(validPassword)
+    
+        app.buttons["Log in"].tap()
+        app.buttons["Resend Code"].tap() 
+        
+    }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    
 
 }
